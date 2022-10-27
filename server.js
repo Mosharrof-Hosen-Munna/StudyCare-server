f274d98b1fec4e754b0c5f4ec10b123c57e2abdd
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2022-10-27 18:30:52 +0600

    create project stucture and create  a server

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require("cors")
+app.use(cors())
+
+const courseCategory = require('./Data/Category.json')
+const allCourses = require("./Data/Courses.json")
+
+app.get('/', (req, res) => {
+    res.json("hello")
+})
+
+app.get('/category', (req, res) => {
+    res.json(courseCategory)
+})
+
+app.get('/courses', (req, res) => {
+    res.json(allCourses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id
+        const categoryCourse = allCourses.filter(course => course._id === id)
+        res.send(categoryCourse)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const singleCourse = allCourses.find(course => course._id === id)
+    res.send(singleCourse)
+})
+
+app.listen(port, () => {
+    console.log("Listening at port", port)
+})
